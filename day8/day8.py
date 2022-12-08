8efd550e2ed5ab7acdcf5b98d310c64a44f84a6c
TwoRice <2rimagowan@gmail.com>
2022-12-08 15:29:21 +0000

    do everything in 1 loop

--- day8/day8.py
+++ day8/day8.py
@@ -1,46 +1,29 @@
-def is_hidden(trees, y, x):
+def get_tree_stats(trees, y, x):
     height = trees[y][x]
     hidden = [False, False, False, False]
-    for i in range(x + 1, len(trees)):
-        if trees[y][i] >= height:
-            hidden[0] = True
-            break
-    for i in range(x - 1, -1, -1):
-        if trees[y][i] >= height:
-            hidden[1] = True
-            break
-    for i in range(y + 1, len(trees)):
-        if trees[i][x] >= height:
-            hidden[2] = True
-            break
-    for i in range(y - 1, -1, -1):
-        if trees[i][x] >= height:
-            hidden[3] = True
-            break
-            
-    return all(hidden)
-
-def calculate_scenic_score(trees, y ,x):
-    height = trees[y][x]
     scores = [0, 0, 0, 0]
     for i in range(x + 1, len(trees)):
         scores[0] += 1
         if trees[y][i] >= height:
+            hidden[0] = True
             break
     for i in range(x - 1, -1, -1):
         scores[1] += 1
         if trees[y][i] >= height:
+            hidden[1] = True
             break
     for i in range(y + 1, len(trees)):
         scores[2] += 1
         if trees[i][x] >= height:
+            hidden[2] = True
             break
     for i in range(y - 1, -1, -1):
         scores[3] += 1
         if trees[i][x] >= height:
+            hidden[3] = True
             break
-    
-    return scores[0] * scores[1] * scores[2] * scores[3]
+            
+    return all(hidden), scores[0] * scores[1] * scores[2] * scores[3]
 
 if __name__ == "__main__":
     with open("day8.txt", "r") as f:
@@ -51,7 +34,8 @@ if __name__ == "__main__":
     scenic_scores = []
     for y in range(1, len(trees) - 1):
         for x in range(1, len(trees) - 1):
-            hidden_trees += is_hidden(trees, y, x)
-            scenic_scores.append(calculate_scenic_score(trees, y, x))
+            hidden, score = get_tree_stats(trees, y, x)
+            hidden_trees += hidden
+            scenic_scores.append(score)
     print(f"Part 1: {len(raw_data) - hidden_trees - len(trees) + 1}")
     print(f"Part 2: {max(scenic_scores)}")
