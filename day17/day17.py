cd7ed23fea69f9c4e3ad77f0b3a61b8b25929857
TwoRice <2rimagowan@gmail.com>
2023-12-14 10:53:39 +0000

    day 17 part 2

--- day17/day17.py
+++ day17/day17.py
@@ -9,6 +9,64 @@ def move_rock(chamber, rock_pos, move):
         return True, chamber, rock_pos
     return False, chamber, rock_pos
 
+def drop_rock(chamber, n, i, empty_space):
+    # Spawn rock
+    chamber = np.concatenate([rocks[n], empty_space, chamber])
+    rock_pos = np.array(np.where(rocks[n] == 1))
+    
+    while True:
+        # Move left or right
+        move = jet_pattern[i % len(jet_pattern)]
+        i += 1
+        if move == ">" and 6 not in rock_pos[1]:
+            _, chmaber, rock_pos = move_rock(chamber, rock_pos, [[0], [1]])
+        elif move == "<" and 0 not in rock_pos[1]:
+            _, chmaber, rock_pos = move_rock(chamber, rock_pos, [[0], [-1]])
+        
+        # move down
+        moved, chmaber, rock_pos = move_rock(chamber, rock_pos, [[1], [0]])
+        
+        # Rock stops
+        if not moved:
+            chamber[rock_pos[0], rock_pos[1]] += 1
+            chamber = chamber[~np.all(chamber == 0, axis=1)]
+            return chamber, i
+
+def simulate_rocks(n):
+    chamber = np.ones((1, 7)) * 2
+    empty_space = np.zeros((3, 7))
+    heights = []
+    rocks_dropped = 0
+    i = 0
+    no_pattern = True
+    
+    for j in range(n):
+        n = rocks_dropped % 5
+        chamber, i = drop_rock(chamber, n, i, empty_space)
+        rocks_dropped += 1
+        heights.append(chamber.shape[0] - 1)
+
+    return heights
+
+def find_pattern(values):
+    n = len(values)
+    longest_pattern = []
+    for pattern_length in range(1, n//2 + 1):
+        for i in range(n - 2*pattern_length + 1):
+            pattern = values[i:i+pattern_length]
+            if values[i:i+pattern_length] == values[i+pattern_length:i+2*pattern_length]:
+                if len(pattern) > len(longest_pattern):
+                    longest_pattern = pattern
+
+    return longest_pattern
+
+def find_sublist(lst, sublist):
+    sub_len = len(sublist)
+    for i in range(len(lst)):
+        if lst[i:i+sub_len] == sublist:
+            return i
+    return -1
+
 if __name__ == "__main__":
     with open("day17.txt", "r") as f:
         jet_pattern = f.read()
@@ -21,40 +79,12 @@ if __name__ == "__main__":
         np.array([[0, 0, 1, 1, 0, 0, 0], [0, 0, 1, 1, 0, 0, 0]]),
     ]
 
-    chamber = np.ones((1, 7)) * 2
-    empty_space = np.zeros((3, 7))
-    total_height = 0
-    i = 0
+    print(f"Part 1: {simulate_rocks(2022)[-1]}")
 
-    for j in range(2022):
-        # Check for new floor
-        new_floors = np.where(np.all(chamber[:-1] == 2, axis=1))[0]
-        if len(new_floors > 0): 
-            total_height += chamber.shape[0] - new_floors[0] - 1
-            chamber = chamber[:new_floors[0] + 1]
-        
-        # Spawn rock
-        n = j % 5
-        chamber = np.concatenate([rocks[n], empty_space, chamber])
-        rock_pos = np.array(np.where(rocks[n] == 1))
-        
-        at_rest = False
-        while not at_rest:
-            # Move left or right
-            move = jet_pattern[i % len(jet_pattern)]
-            i += 1
-            if move == ">" and 6 not in rock_pos[1]:
-                _, chmaber, rock_pos = move_rock(chamber, rock_pos, [[0], [1]])
-            elif move == "<" and 0 not in rock_pos[1]:
-                _, chmaber, rock_pos = move_rock(chamber, rock_pos, [[0], [-1]])
-            
-            # move down
-            moved, chmaber, rock_pos = move_rock(chamber, rock_pos, [[1], [0]])
-            
-            # Rock stops
-            if not moved:
-                chamber[rock_pos[0], rock_pos[1]] += 1
-                at_rest = True
-                chamber = chamber[~np.all(chamber == 0, axis=1)]
-
-    print(f"Part 1: {total_height + chamber.shape[0] - 1}")
+    heights = simulate_rocks(5000)
+    height_diffs = np.diff(heights).tolist()
+    pattern = find_pattern(height_diffs)
+    offset = find_sublist(height_diffs, pattern)
+    cycles = (1000000000000 - offset) // len(pattern)
+    remainder  = (1000000000000 - offset) % len(pattern)
+    print(f"Part 1: {heights[offset] + (sum(pattern) * cycles) + sum(pattern[:remainder-1])}")
